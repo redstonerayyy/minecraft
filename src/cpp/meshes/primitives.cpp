78fccf62839a29942c516971b7ca1b514aea9179
Redstonerayy <redstone.rayy@gmail.com>
2022-07-25 20:29:26 +0200

    lighting maybe

--- src/cpp/meshes/primitives.cpp
+++ src/cpp/meshes/primitives.cpp
@@ -8,7 +8,7 @@ Vertex createVertex(const float * position, float tex1, float tex2, float nvecx,
 	return vert;
 }
 
-void generateSquare(std::vector<Vertex> &vertices, std::vector<unsigned int> &indices, const float squarecords[4][3], const float cordshift[3], float pvector[3]){
+void generateSquare(std::vector<Vertex> &vertices, std::vector<unsigned int> &indices, const float squarecords[4][3], const float cordshift[3], float pvector[3], const float normal[3]){
 	int verticeslength = vertices.size();
 	glm::mat4 transmat = glm::translate(glm::mat4(1.0f), glm::vec3(pvector[0], pvector[1], pvector[2]));
 	float squarecordsnew[4][3];
@@ -22,10 +22,10 @@ void generateSquare(std::vector<Vertex> &vertices, std::vector<unsigned int> &in
 		squarecordsnew[i][2] = temp.z;
 	}
 	//create vertices
-	vertices.push_back(createVertex(squarecordsnew[0], defaulttexcoords[0][0], defaulttexcoords[0][1], 1.0f, 1.0f, 1.0f));
-	vertices.push_back(createVertex(squarecordsnew[1], defaulttexcoords[1][0], defaulttexcoords[1][1], 1.0f, 1.0f, 1.0f));
-	vertices.push_back(createVertex(squarecordsnew[2], defaulttexcoords[2][0], defaulttexcoords[2][1], 1.0f, 1.0f, 1.0f));
-	vertices.push_back(createVertex(squarecordsnew[3], defaulttexcoords[3][0], defaulttexcoords[3][1], 1.0f, 1.0f, 1.0f));
+	vertices.push_back(createVertex(squarecordsnew[0], defaulttexcoords[0][0], defaulttexcoords[0][1], normal[0], normal[1], normal[2]));
+	vertices.push_back(createVertex(squarecordsnew[1], defaulttexcoords[1][0], defaulttexcoords[1][1], normal[0], normal[1], normal[2]));
+	vertices.push_back(createVertex(squarecordsnew[2], defaulttexcoords[2][0], defaulttexcoords[2][1], normal[0], normal[1], normal[2]));
+	vertices.push_back(createVertex(squarecordsnew[3], defaulttexcoords[3][0], defaulttexcoords[3][1], normal[0], normal[1], normal[2]));
 	//first triangle
 	indices.push_back(verticeslength);
 	indices.push_back(verticeslength + 1);
@@ -42,21 +42,21 @@ void generateCube(std::vector<Vertex> &vertices, std::vector<unsigned int> &mesh
 	const float shifty[3] = {0.0f, 1.0f, 0.0f};
 	const float shiftz[3] = {0.0f, 0.0f, -1.0f};
 	if(sides[0]){//left
-		generateSquare(vertices, mesh_indices, squarex, shiftzero, pvector);
+		generateSquare(vertices, mesh_indices, squarex, shiftzero, pvector, normals[0]);
 	}
 	if(sides[1]){//bottom
-		generateSquare(vertices, mesh_indices, squarey, shiftzero, pvector);
+		generateSquare(vertices, mesh_indices, squarey, shiftzero, pvector, normals[1]);
 	}
 	if(sides[2]){//front
-		generateSquare(vertices, mesh_indices, squarez, shiftzero, pvector);
+		generateSquare(vertices, mesh_indices, squarez, shiftzero, pvector, normals[2]);
 	}
 	if(sides[3]){//left
-		generateSquare(vertices, mesh_indices, squarex, shiftx, pvector);
+		generateSquare(vertices, mesh_indices, squarex, shiftx, pvector, normalsreverse[0]);
 	}
 	if(sides[4]){//top
-		generateSquare(vertices, mesh_indices, squarey, shifty, pvector);
+		generateSquare(vertices, mesh_indices, squarey, shifty, pvector, normalsreverse[1]);
 	}
 	if(sides[5]){//back
-		generateSquare(vertices, mesh_indices, squarez, shiftz, pvector);
+		generateSquare(vertices, mesh_indices, squarez, shiftz, pvector, normalsreverse[2]);
 	}
 }
--- src/include/meshes/primitives.h
+++ src/include/meshes/primitives.h
@@ -15,7 +15,7 @@
 //add primitive shapes to a meshes buffer data
 //take a vertex array pointer and a indice array pointer
 Vertex createVertex(float x, float y, float z, float tex1, float tex2, float nvecx, float nvecz, float nvecy);
-void generateSquare(std::vector<Vertex> &vertices, std::vector<unsigned int> &indices, const float squarecords[4][3], const float cordshift[3], float pvector[3]);
+void generateSquare(std::vector<Vertex> &vertices, std::vector<unsigned int> &indices, const float squarecords[4][3], const float cordshift[3], float pvector[3], const float normal[3]);
 void generateCube(std::vector<Vertex> &vertices, std::vector<unsigned int> &mesh_indices, float pvector[3], int sides[6]);
 
 #endif
