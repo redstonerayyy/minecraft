f165a05b059c4c14bbaccd1c9b99479de23a660b
Redstonerayy <antonpaulr@protonmail.com>
2022-10-09 22:43:50 +0200

    mesh class simplified, now building again

--- src/include/meshes/mesh.h
+++ src/include/meshes/mesh.h
@@ -9,24 +9,15 @@
 class Mesh {
 public:
     //MESH DATA, vertices and indices
-    std::vector<std::vector<Vertex>> verticesarrays;
-    std::vector<std::vector<unsigned int> indicesarrays;
-    
-    void addVerticeArray(std::vector<Vertex> vertices);
-    void addIndiceArray(std::vector<unsigned int> indices);
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
 
-    std::vector<Vertex> ConcatVerticeArrays(std::vector<int> indices);
-    std::vector<unsigned int> ConcatIndiceArrays(std::vector<int> indices);
-    
-    //VAOS
-    std::vector<VAO> vaos;
-    
-    void addVAO(std::vector<int> varrays, std::vector<int> iarrays, std::string name);
-    void deleteVAO(std::string name);
-    void bindVAO(std::string name);
-    void drawVAO(ShaderProgram &shaderprogram);
+    VAO* vao = new VAO();
 
-    Mesh();
+    void generateBuffers(bool pos = true, bool tex  = true, bool normal  = true);
+    void draw(ShaderProgram &shaderprogram);
+
+    Mesh(std::vector<Vertex> verts, std::vector<unsigned int> inds);
 };
 
 #endif
