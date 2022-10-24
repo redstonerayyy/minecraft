a6a8c6bd95a1821e4292eb6e13275ddd3d822fe0
Redstonerayy <redstone.rayy@gmail.com>
2022-10-24 14:42:25 +0200

    fixed

--- src/cpp/game/world/world.cpp
+++ src/cpp/game/world/world.cpp
@@ -10,21 +10,20 @@ World::World(unsigned long seed){
 
 void World::SetSeed(unsigned long seed){
     this->seed = seed;
-    this->perlinnoiseseed = seed;
-    this->perlinnoise = ;
-
 }
 
 std::vector<std::vector<float>> World::SampleChunkNoise(int xoffset, int zoffset){
     //2 dimensional vector
     std::vector<std::vector<float>> noisemap;
+    // make noise object
+    siv::PerlinNoise perlinnoise { this->seed };
     //scale and octaves as parameters
     for (int x = 0; x < this->chunksize; x++) {
         // fill vector with noise floats between 0 and 1
         noisemap.push_back(std::vector<float>{});
         for (int z = 0; z < this->chunksize; z++) {
             // sample noise at given position
-            float noise = this->perlinnoise.octave2D_01(
+            float noise = perlinnoise.octave2D_01(
                     ((xoffset * this->chunksize) + x) * this->scale,
                     ((zoffset * this->chunksize) + z) * this->scale,
                     this->octaves
